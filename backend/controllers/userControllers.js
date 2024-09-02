e83369712ebc1d62d20550df1777c2e30f41a079
Devraj-singh-tomar <devrajsingh.t11@gmail.com>
2024-09-02 17:25:24 +0530

    feat: created search APIs and all chat APIs

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -56,4 +56,20 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { registerUser, authUser };
+// /api/user?search=devraj
+const allUsers = asyncHandler(async (req, res) => {
+  const keyword = req.query.search
+    ? {
+        $or: [
+          { name: { $regex: req.query.search, $options: "i" } },
+          { email: { $regex: req.query.search, $options: "i" } },
+        ],
+      }
+    : {};
+
+  // this will not find the loggedIN user
+  const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
+  res.send(users);
+});
+
+module.exports = { registerUser, authUser, allUsers };
