991c37f2f6655f368773f3343c1b3679d05af490
Devraj-singh-tomar <devrajsingh.t11@gmail.com>
2024-09-03 18:24:35 +0530

    implement state management

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   Box,
   Container,
@@ -11,8 +11,19 @@ import {
 } from "@chakra-ui/react";
 import Login from "../components/Authentication/Login";
 import Signup from "../components/Authentication/Signup";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("userInfo"));
+
+    if (user) {
+      navigate("/chats");
+    }
+  }, [navigate]);
+
   return (
     <Container maxWidth="xl" centerContent>
       <Box
